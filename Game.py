510d9a6bd2e309b22b67633ab4b7a028cf25cf7d
VSMikhail <116807633+VSMikhail@users.noreply.github.com>
2024-05-14 16:26:04 +0700

    Initial code in main.py

--- Game.py
+++ Game.py
@@ -14,6 +14,8 @@ class GameFool:
         self.deck = [Cards(rank, suit, 0) for rank in range(self.deck_start, 15) for suit in range(1, 5)]
         self.cards_in_hands = [[] for _ in range(0, self.players_number)]
         self.beaten_cards = []
+        self.cards_on_desk = [[], []]
+        self.trumps = None
 
     def add_cards_to_hands(self):
         for _ in self.cards_in_hands:
@@ -22,47 +24,55 @@ class GameFool:
     def deal_cards(self):
         random.shuffle(self.deck)
         self.deck = [Cards(card.rank, card.suit, 1) if card.suit == self.deck[0].suit else card for card in self.deck ]
-        self.add_cards_to_hands()
         print(f'|X----------X|\n|X    XX    X|\n|X__________X|\n  {self.deck[0]}')
+        self.trumps = self.deck[0].suit
+        self.add_cards_to_hands()
+
+    def print_desk(self):
+        print(f'{self.deck[0].symbols[1][self.trumps - 1]} are trumps')
+        print(f'1 player\n' + '    '.join(['     ' + str(_) for _ in range(0, len(self.cards_in_hands[0]))]))
+        for _ in self.cards_in_hands[0]:
+            print(_, end='')
+        print('\n\n')
+        for _ in self.cards_on_desk[0]:
+            print(_, end='')
+        print('')
+        for _ in self.cards_on_desk[1]:
+            print(_, end='')
+        print('\n\n')
+        for _ in self.cards_in_hands[1]:
+            print(_, end='')
+        print('\n' + '    '.join(
+            ['     ' + str(_) for _ in range(0, len(self.cards_in_hands[1]))]) + f'\n2 player')
 
     def play_round(self, who_goes, who_beat):
         end_round = True
-        cards_on_desk = [[], []]
         while end_round:
-            print('First player\n' + '    '.join(['     ' + str(_) for _ in range(0, len(self.cards_in_hands[who_goes]))]))
-            for _ in self.cards_in_hands[who_goes]:
-                print(_, end='')
-            print('\n\n')
-            for _ in cards_on_desk[0]:
-                print(_, end='')
-            print('')
-            for _ in cards_on_desk[1]:
-                print(_, end='')
-            print('\n\n')
-            for _ in self.cards_in_hands[who_beat]:
-                print(_, end='')
-            print('\n' + '    '.join(['     ' + str(_) for _ in range(0, len(self.cards_in_hands[who_beat]))]) + '\nSecond player')
-            first_choice = int(input('First player goes: '))
+            self.print_desk()
+            first_choice = int(input(f'{who_goes + 1} player goes: '))
             if first_choice == 100:
                 print('All beaten!')
-                move_cards(cards_on_desk[0], range(0, len(cards_on_desk[0])), self.beaten_cards)
-                move_cards(cards_on_desk[1], range(0, len(cards_on_desk[1])), self.beaten_cards)
+                move_cards(self.cards_on_desk[0], range(0, len(self.cards_on_desk[0])), self.beaten_cards)
+                move_cards(self.cards_on_desk[1], range(0, len(self.cards_on_desk[1])), self.beaten_cards)
                 break
             first_player_card = self.cards_in_hands[who_goes][first_choice]
-            move_cards(self.cards_in_hands[who_goes], [first_choice], cards_on_desk[0])
-            if first_player_card in cards_on_desk[0] or first_player_card in cards_on_desk[1] or not cards_on_desk[0]:
+            if first_player_card in self.cards_on_desk[0] or first_player_card in self.cards_on_desk[1] or not self.cards_on_desk[0]:
+                move_cards(self.cards_in_hands[who_goes], [first_choice], self.cards_on_desk[0])
+                self.print_desk()
                 while True:
-                    second_choice = int(input('Second player goes: '))
+                    second_choice = int(input(f'{who_beat + 1} player goes: '))
                     if second_choice == 100:
                         print('Take all!')
-                        move_cards(cards_on_desk[0], range(0, len(cards_on_desk[0])), self.cards_in_hands[who_beat])
-                        move_cards(cards_on_desk[1], range(0, len(cards_on_desk[1])), self.cards_in_hands[who_beat])
+                        move_cards(self.cards_on_desk[0], range(0, len(self.cards_on_desk[0])),
+                                   self.cards_in_hands[who_beat])
+                        move_cards(self.cards_on_desk[1], range(0, len(self.cards_on_desk[1])),
+                                   self.cards_in_hands[who_beat])
                         end_round = False
                         break
                     second_player_card = self.cards_in_hands[who_beat][second_choice]
                     if second_player_card > first_player_card:
                         print('Second card win')
-                        move_cards(self.cards_in_hands[who_beat], [second_choice], cards_on_desk[1])
+                        move_cards(self.cards_in_hands[who_beat], [second_choice], self.cards_on_desk[1])
                         break
                     elif second_player_card < first_player_card:
                         print('Wrong card')
--- main.py
+++ main.py
@@ -5,7 +5,15 @@ from Cards import Cards
 
 game = GameFool(6, 2)
 game.deal_cards()
-game.play_round(0, 1)
+end_game = True
+beat, goes = 0, 1
+while end_game:
+    start_beaten_cards = len(game.beaten_cards)
+    game.play_round(beat, goes)
+    if len(game.beaten_cards) > start_beaten_cards:
+        beat, goes = goes, beat
+    game.add_cards_to_hands()
+
 print('\nКарты первого')
 for _ in game.cards_in_hands[0]:
     print(_)
