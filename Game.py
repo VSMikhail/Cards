ba4a443ba495d932824ad9e6ec5409bf861c8e5b
VSMikhail <116807633+VSMikhail@users.noreply.github.com>
2024-05-15 16:53:43 +0700

    Complete game for two players

--- Game.py
+++ Game.py
@@ -1,5 +1,4 @@
 from Cards import Cards
-import numpy as np
 import random
 
 
@@ -19,17 +18,25 @@ class GameFool:
 
     def add_cards_to_hands(self):
         for _ in self.cards_in_hands:
-            move_cards(self.deck, range(0, len(_) - 6, -1), _)
+            cards_needed = len(_) - 6
+            if len(self.deck) >= 6:
+                move_cards(self.deck, range(0, cards_needed, -1), _)
+            elif (len(self.deck) + cards_needed) >= 0:
+                move_cards(self.deck, range(0, cards_needed, -1), _)
+            elif (len(self.deck) + cards_needed) < 0:
+                move_cards(self.deck, range(0, - len(self.deck), -1), _)
+            else:
+                break
 
     def deal_cards(self):
         random.shuffle(self.deck)
         self.deck = [Cards(card.rank, card.suit, 1) if card.suit == self.deck[0].suit else card for card in self.deck ]
         print(f'|X----------X|\n|X    XX    X|\n|X__________X|\n  {self.deck[0]}')
-        self.trumps = self.deck[0].suit
+        self.trumps = self.deck[0].symbols[1][self.deck[0].suit - 1]
         self.add_cards_to_hands()
 
     def print_desk(self):
-        print(f'{self.deck[0].symbols[1][self.trumps - 1]} are trumps')
+        print(f'{self.trumps} are trumps')
         print(f'1 player\n' + '    '.join(['     ' + str(_) for _ in range(0, len(self.cards_in_hands[0]))]))
         for _ in self.cards_in_hands[0]:
             print(_, end='')
@@ -46,8 +53,8 @@ class GameFool:
             ['     ' + str(_) for _ in range(0, len(self.cards_in_hands[1]))]) + f'\n2 player')
 
     def play_round(self, who_goes, who_beat):
-        end_round = True
-        while end_round:
+        continue_round = True
+        while continue_round:
             self.print_desk()
             first_choice = int(input(f'{who_goes + 1} player goes: '))
             if first_choice == 100:
@@ -63,15 +70,25 @@ class GameFool:
                     second_choice = int(input(f'{who_beat + 1} player goes: '))
                     if second_choice == 100:
                         print('Take all!')
+                        while True:
+                            first_choice = int(input(f'{who_goes + 1} player toss: '))
+                            if first_choice == 100:
+                                print('Will not')
+                                break
+                            first_player_card = self.cards_in_hands[who_goes][first_choice]
+                            if first_player_card in self.cards_on_desk[0] or first_player_card in self.cards_on_desk[
+                                1] or not self.cards_on_desk[0]:
+                                move_cards(self.cards_in_hands[who_goes], [first_choice], self.cards_on_desk[0])
+                                self.print_desk()
+
                         move_cards(self.cards_on_desk[0], range(0, len(self.cards_on_desk[0])),
                                    self.cards_in_hands[who_beat])
                         move_cards(self.cards_on_desk[1], range(0, len(self.cards_on_desk[1])),
                                    self.cards_in_hands[who_beat])
-                        end_round = False
+                        continue_round = False
                         break
                     second_player_card = self.cards_in_hands[who_beat][second_choice]
                     if second_player_card > first_player_card:
-                        print('Second card win')
                         move_cards(self.cards_in_hands[who_beat], [second_choice], self.cards_on_desk[1])
                         break
                     elif second_player_card < first_player_card:
--- main.py
+++ main.py
@@ -1,25 +1,16 @@
-from pprint import pprint
 from Game import GameFool
-from Cards import Cards
 
 
-game = GameFool(6, 2)
+game = GameFool(11, 2)
 game.deal_cards()
-end_game = True
-beat, goes = 0, 1
-while end_game:
+continue_game = True
+goes, beat = 0, 1
+while continue_game:
     start_beaten_cards = len(game.beaten_cards)
-    game.play_round(beat, goes)
+    game.play_round(goes, beat)
     if len(game.beaten_cards) > start_beaten_cards:
         beat, goes = goes, beat
     game.add_cards_to_hands()
-
-print('\nКарты первого')
-for _ in game.cards_in_hands[0]:
-    print(_)
-print('\nКарты второго')
-for _ in game.cards_in_hands[1]:
-    print(_)
-print('\nБито')
-for _ in game.beaten_cards:
-    print(_)
+    if not game.cards_in_hands[goes]:
+        print(f'{goes + 1} player win!')
+        continue_game = False
