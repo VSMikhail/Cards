dbbad7605fcd973e04ace027ec52262aea07e4b3
VSMikhail <116807633+VSMikhail@users.noreply.github.com>
2024-05-15 17:55:09 +0700

    Add check for inputs

--- Game.py
+++ Game.py
@@ -35,9 +35,17 @@ class GameFool:
         self.trumps = self.deck[0].symbols[1][self.deck[0].suit - 1]
         self.add_cards_to_hands()
 
+    def check_input(self, input_str, player):
+        if input_str.isdigit():
+            input_int = int(input_str)
+            if 0 < input_int <= len(self.cards_in_hands[player]) or input_int == 100:
+                return input_int
+        else:
+            return None
+
     def print_desk(self):
         print(f'{self.trumps} are trumps')
-        print(f'1 player\n' + '    '.join(['     ' + str(_) for _ in range(0, len(self.cards_in_hands[0]))]))
+        print(f'1 player\n' + '    '.join(['     ' + str(_) for _ in range(1, len(self.cards_in_hands[0]) + 1)]))
         for _ in self.cards_in_hands[0]:
             print(_, end='')
         print('\n\n')
@@ -50,35 +58,44 @@ class GameFool:
         for _ in self.cards_in_hands[1]:
             print(_, end='')
         print('\n' + '    '.join(
-            ['     ' + str(_) for _ in range(0, len(self.cards_in_hands[1]))]) + f'\n2 player')
+            ['     ' + str(_) for _ in range(1, len(self.cards_in_hands[1]) + 1)]) + f'\n2 player')
 
     def play_round(self, who_goes, who_beat):
         continue_round = True
         while continue_round:
             self.print_desk()
-            first_choice = int(input(f'{who_goes + 1} player goes: '))
+            while True:
+                first_choice = self.check_input(input(f'{who_goes + 1} player goes: '), who_goes)
+                if first_choice:
+                    break
             if first_choice == 100:
                 print('All beaten!')
                 move_cards(self.cards_on_desk[0], range(0, len(self.cards_on_desk[0])), self.beaten_cards)
                 move_cards(self.cards_on_desk[1], range(0, len(self.cards_on_desk[1])), self.beaten_cards)
                 break
-            first_player_card = self.cards_in_hands[who_goes][first_choice]
+            first_player_card = self.cards_in_hands[who_goes][first_choice - 1]
             if first_player_card in self.cards_on_desk[0] or first_player_card in self.cards_on_desk[1] or not self.cards_on_desk[0]:
-                move_cards(self.cards_in_hands[who_goes], [first_choice], self.cards_on_desk[0])
+                move_cards(self.cards_in_hands[who_goes], [first_choice - 1], self.cards_on_desk[0])
                 self.print_desk()
                 while True:
-                    second_choice = int(input(f'{who_beat + 1} player goes: '))
+                    while True:
+                        second_choice = self.check_input(input(f'{who_beat + 1} player goes: '), who_beat)
+                        if second_choice:
+                            break
                     if second_choice == 100:
                         print('Take all!')
                         while True:
-                            first_choice = int(input(f'{who_goes + 1} player toss: '))
+                            while True:
+                                first_choice = self.check_input(input(f'{who_goes + 1} player toss: '), who_goes)
+                                if first_choice:
+                                    break
                             if first_choice == 100:
                                 print('Will not')
                                 break
-                            first_player_card = self.cards_in_hands[who_goes][first_choice]
+                            first_player_card = self.cards_in_hands[who_goes][first_choice - 1]
                             if first_player_card in self.cards_on_desk[0] or first_player_card in self.cards_on_desk[
                                 1] or not self.cards_on_desk[0]:
-                                move_cards(self.cards_in_hands[who_goes], [first_choice], self.cards_on_desk[0])
+                                move_cards(self.cards_in_hands[who_goes], [first_choice - 1], self.cards_on_desk[0])
                                 self.print_desk()
 
                         move_cards(self.cards_on_desk[0], range(0, len(self.cards_on_desk[0])),
@@ -87,9 +104,9 @@ class GameFool:
                                    self.cards_in_hands[who_beat])
                         continue_round = False
                         break
-                    second_player_card = self.cards_in_hands[who_beat][second_choice]
+                    second_player_card = self.cards_in_hands[who_beat][second_choice - 1]
                     if second_player_card > first_player_card:
-                        move_cards(self.cards_in_hands[who_beat], [second_choice], self.cards_on_desk[1])
+                        move_cards(self.cards_in_hands[who_beat], [second_choice - 1], self.cards_on_desk[1])
                         break
                     elif second_player_card < first_player_card:
                         print('Wrong card')
